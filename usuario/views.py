68ed5b68878e3a0f0ba5c2e24453712157e30dd8
Joaquin <joa.zamorano@alumnos.duoc.cl>
2021-01-21 12:12:20 -0300

    urls y vistas

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'usuario/inicio.html', {})
+def lista(request):
+    return render(request, 'usuario/lista_persona.html', {})
+    
+#def acceso(request):
+'''    m = Member.objects.get(username=request.POST['username'])
+    if m.password == request.POST['password']:
+        request.session['member_id'] = m.id
+        return HttpResponse("Usted está logeado")
+    else:
+        return HttpResponse("Tu usuario o contraseña no son correctos")
+
+def logout(request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+    return HttpResponse("Sesión cerrada")'''
