fd5ba95855bdc874fbe850bb0e869722103d7592
Joaquin <joa.zamorano@alumnos.duoc.cl>
2021-01-27 15:01:50 -0300

    carpeta templates, urls y vistas

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path
-from . import views
-# Vistas propias de Django, para login y logout
+from django.urls import path, include
 from django.contrib.auth.views import LoginView,LogoutView
-
-
+from .views import index, admin, habitaciones, login, logout, registro, reservar
+from usuario import views
+#from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('lista/', views.lista, name='lista'),
-    path('login/',LoginView.as_view(template_name='usuario/login.html'), name="InicioSesion"),
-    path('logout/',LogoutView.as_view(template_name='usuario/inicio.html'),name="Logout"), 
+    path('',views.index, name='index'),
+    path('index.html/', views.index , name='index'),
+    path('admin.html/', views.admin, name='admin'),
+    path('login/',LoginView.as_view(template_name='usuario/index.html/login.html'), name="login"),
+    path('logout/',LogoutView.as_view(template_name='usuario/index.html'),name="logout"), 
+    path('registro.html/', views.registro, name='registro'),
+    path('reservar.html/',views.reservar, name='reservar'),
+    path('habitaciones.html/', views.habitaciones, name='habitaciones')
 ]
 
